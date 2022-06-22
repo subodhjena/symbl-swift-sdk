6432774a0d464652114c2caa5b45a6fe24db3a7b
Subodh Jena <jenasubodh@gmail.com>
2022-06-22 15:31:45 +0530

    Initial Commit

---
+++ Sources/SymblSwiftSDK/SymblRealtimeApi.swift
@@ -0,0 +1,12 @@
+//
+//  SymblRealtime.swift
+//  
+//
+//  Created by Subodh Jena on 21/06/22.
+//
+
+import Foundation
+
+public struct SymblRealtimeApi {
+    
+}
--- Sources/SymblSwiftSDK/SymblSwiftSDK.swift
+++ Sources/SymblSwiftSDK/SymblSwiftSDK.swift
@@ -1,6 +1,30 @@
-public struct SymblSwiftSDK {
-    public private(set) var text = "Hello, World!"
+//
+//  SymblSwiftSDK.swift
+//
+//
+//  Created by Subodh Jena on 21/06/22.
+//
 
-    public init() {
+import Foundation
+
+public struct Symbl {
+    private var _acessToken = ""
+    
+    public init(accessToken: String) {
+        _acessToken = accessToken
     }
+    
+    public func initializeRealtimeApi() throws -> SymblRealtimeApi {
+        if(_acessToken.isEmpty) {
+            throw SymblError.invalidAccessToken
+        }
+        
+        return SymblRealtimeApi()
+    }
+}
+
+
+enum SymblError: Error {
+    case invalidAccessToken
+    case expiredAccessToken
 }
